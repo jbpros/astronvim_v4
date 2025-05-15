7f0de0721641089bc798ff5b9be12e0d15ad71b7
Julien Biezemans <jb@jbpros.com>
2025-05-15 22:08:08 +0200

    Start eslint_d only when eslint config is present

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,6 +9,43 @@ return {
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
+    local u = require "null-ls.utils"
+
+    -- Function to check for ESLint config files
+    local function has_eslint_config(utils)
+      -- Check for ESLint flat config files (new ESLint 9+ format)
+      if utils.root_has_file({
+        'eslint.config.js',
+        'eslint.config.mjs',
+        'eslint.config.cjs',
+      }) then
+        return true
+      end
+
+      -- Check for traditional ESLint config files
+      if utils.root_has_file({
+        '.eslintrc',
+        '.eslintrc.js',
+        '.eslintrc.cjs',
+        '.eslintrc.yaml',
+        '.eslintrc.yml',
+        '.eslintrc.json',
+      }) then
+        return true
+      end
+
+      -- Check package.json for eslint config
+      local package_json = utils.root_has_file("package.json")
+      if package_json then
+        local ok, package_content = pcall(vim.fn.json_decode, vim.fn.readfile(package_json))
+        if ok then
+          -- Only check for the eslintConfig field
+          return package_content.eslintConfig ~= nil
+        end
+      end
+
+      return false
+    end
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -17,9 +54,15 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
-      require("none-ls.code_actions.eslint_d"),
-      require("none-ls.diagnostics.eslint_d"),
-      require("none-ls.formatting.eslint_d"),
+      require("none-ls.code_actions.eslint_d").with({
+        condition = has_eslint_config,
+      }),
+      require("none-ls.diagnostics.eslint_d").with({
+        condition = has_eslint_config,
+      }),
+      require("none-ls.formatting.eslint_d").with({
+        condition = has_eslint_config,
+      }),
     }
     return config -- return final config table
   end,
